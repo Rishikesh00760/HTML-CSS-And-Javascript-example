778472111cc9006c6f00de9af87ceab73e537920
Rishikesh00760 <94601360+Rishikesh00760@users.noreply.github.com>
2021-11-22 15:27:11 +0530

    Add files via upload

---
+++ HTML, CSS, Javascript Example/function.js
@@ -0,0 +1,4 @@
+function clicked() {
+  alert("The Color Of The Button Is Changed To Blue");
+  document.getElementById("about").style.color = "blue";
+}
---
+++ HTML, CSS, Javascript Example/script.js
@@ -0,0 +1,4 @@
+function cli() {
+  prompt("What Is Your Name");
+  document.getElementById("prom").innerText = "Hello";
+}
